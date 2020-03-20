2c804beaca771fbae7697c8ce397a3c1b91ff693
letrung87 <letrung87@MiddleLee>
2020-03-19 21:39:56 -0700

    add mvc code

---
+++ SimStationProject/src/mvc/View.java
@@ -0,0 +1,41 @@
+package mvc;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.JComponent;
+import javax.swing.border.LineBorder;
+
+public class View extends JComponent implements PropertyChangeListener{
+	protected Model model;
+	
+	public View(Model model) {
+		super();
+		this.model = model;
+		model.addPropertyChangeListener(this);		// add myself to the listener of model
+		//set border arount the view component
+		setBorder(LineBorder.createBlackLineBorder());
+	}
+	
+	public Model getModel() {
+		return model;
+	}
+	
+	// called by File/Open and File/New
+	public void setModel(Model model) {
+		if(this.model != null) {
+			this.model.removePropertyChangeListener(this);
+		}	
+		this.model = model;
+		if(model != null) {
+			this.model.addPropertyChangeListener(this);
+			repaint();
+		}	
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		repaint();
+	}
+
+	
+}
