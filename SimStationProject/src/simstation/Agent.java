28f683239057301b3114afc5c3436baafb6eb2e9
letrung87 <letrung87@MiddleLee>
2020-04-07 23:43:05 -0700

    Trung: - first add Agent.java - edit Simulation.java

---
+++ SimStationProject/src/simstation/Agent.java
@@ -0,0 +1,112 @@
+package simstation;
+
+import java.io.Serializable;
+import java.lang.Thread.State;
+
+import mvc.*;
+
+abstract class Agent implements Runnable, Serializable {
+	private static final long serialVersionUID = 1L;
+	private Thread thread;
+	protected String name;
+	protected Simulation world;
+	protected int xc, yc;
+	protected Heading heading;
+	private AgentState state, goal;
+
+	public String getName() {
+		return name;
+	}
+
+	public void join() throws InterruptedException {
+		if (thread != null) thread.join();
+	}
+
+	public Agent(String name) {
+		this.name = name;
+		state = AgentState.READY;
+		thread = null;
+		xc = Utilities.rng.nextInt(Simulation.SIZE);
+		yc = Utilities.rng.nextInt(Simulation.SIZE);
+		heading = Heading.random();
+	}
+
+	public Agent() {
+		this("Agent_ " + mvc.Utilities.getID());
+	}
+
+	public void run() {
+		thread = Thread.currentThread(); // catch my thread
+		while(!isStopped()) {
+			state = AgentState.RUNNING;
+			update();
+			if (goal == state) stop();
+			try {
+				Thread.sleep(100); // be cooperative
+				synchronized(this) {
+					while(isSuspended()) { wait(); }		// waiting for notify
+				}
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+
+	public synchronized void start() {
+		onStart();
+		state = AgentState.READY;
+		if(thread == null) {
+			thread = new Thread(this, name);
+		}
+		thread.start();		// calls run()
+	}
+
+	public abstract void update();
+
+	protected synchronized void onStart() {
+		System.out.println(name + " is starting!!!");
+	}
+
+	protected synchronized void onExit() {
+		System.out.println(name + " is exiting!!!");
+	}
+
+	protected synchronized void onInterrupted() {
+		System.out.println(name + " is interrupted!!!");
+	}
+
+	// state changing functions
+	public synchronized void suspend() { state = AgentState.SUSPENDED; }
+
+	public synchronized void resume() {
+		if (!isStopped()) {
+			state = AgentState.RUNNING;
+			notify();
+		}
+	}
+
+	public synchronized void stop() { state = AgentState.STOPPED; }
+
+	public synchronized boolean isStopped() { return state == AgentState.STOPPED; }
+
+	public boolean isSuspended() { return state == AgentState.SUSPENDED;  }
+
+	public Simulation getWorld() {
+		return world;
+	}
+
+	public void setWorld(Simulation world) {
+		this.world = world;
+	}
+
+	// get distance from this agent to agent a
+	public double distance(Agent a) {
+		// find distance by sqrt((a.xc - this.xc)^2 + (a.yc - this.yc)^2)
+		return Math.sqrt(Math.pow((double)(a.xc - this.xc), 2) + Math.pow((double)(a.yc - this.yc), 2));
+	}
+
+	// test
+	public synchronized boolean finished() {
+		return (state == AgentState.STOPPED) || (thread != null && thread.getState() != State.RUNNABLE);
+	}
+}	
--- SimStationProject/src/simstation/Simulation.java
+++ SimStationProject/src/simstation/Simulation.java
@@ -2,30 +2,136 @@
 package simstation;
 
 import java.util.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import mvc.*;
 
 public class Simulation extends Model {
 
+	public static int SIZE = 250;
 	private Timer timer;
 	private int clock;
+	private Agent agent;
+	private ArrayList<Agent> agents = new ArrayList<Agent>();
 
+	public Simulation()
+	{
+		agents = new ArrayList<Agent>();
+		clock = 0;
+	}
+	
+	public void addAgent(Agent a)
+	{
+		agents.add(a);
+		a.setWorld(this);
+	}
+	
+	public synchronized Agent getNeighbor(Agent asker, double radius)
+	{
+		Agent neighor = null;
+		boolean found = false;
+		int i = Utilities.rng.nextInt(agents.size());
+		int start = i;
+		while(!found) {
+			Agent candidate = agents.get(i);
+			
+			if(candidate != asker && asker.distance(candidate) < radius) 
+			{
+				neighor = agents.get(i);
+				found = true;
+			}
+			else
+			{
+				i = (i + 1) % agents.size();
+				if(i == start) break;
+			}
+		}
+		return neighor;
+	}
+	
 	private void startTimer() {
-	   timer = new Timer();
-       timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
-    }
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+	}
 
-    private void stopTimer() {
-	  timer.cancel();
-	  timer.purge();
-  }
+	private void stopTimer() {
+		timer.cancel();
+		timer.purge();	// remove all cancelled tasks from this timer' task queue.
+	}
 
-  private class ClockUpdater extends TimerTask {
-	  public void run() {
-		  clock++;
-		  //changed();
-      }
-  }
+	class ClockUpdater extends TimerTask {
+		public void run() {
+			clock++;
+		}
+	}
 
-  // etc.
+	//Method start() when click Start button
+	public void start()
+	{
+		agents = new ArrayList<Agent>();
+		clock = 0;
+		populate();
+		startTimer();
+		for(Agent a : agents)
+			a.start();
+	}
+	
+	// override in subclass
+	public void populate() {}
+
+	// Method suspend() when click Suspend button
+	public synchronized void suspend()
+	{
+		for(Agent a: agents)
+		{
+			a.suspend();
+		}
+		stopTimer();
+	}
+
+	// Method name: resume() when click resume button
+	public synchronized void resume()
+	{
+		startTimer();
+		for(Agent a : agents)
+		{
+			a.resume();
+		}
+	}
+
+	// Method name: stop()
+	public synchronized void stop()
+	{
+		for(Agent a : agents)
+		{
+			a.stop();
+		}
+		stopTimer();
+	}
+	
+	public synchronized int getClock() {
+		return clock;
+	}
+	
+	public synchronized Iterator<Agent> iterator(){
+		return agents.iterator(); 
+	}
+	
+	private synchronized void incClock() 
+	{
+		clock++;
+		changed();
+	}
+
+	// method name: stats()
+	public void stats()
+	{
+		JFrame frame = new JFrame("Status");
+		JOptionPane.showMessageDialog(null, "Clicked on Stats button");
+	}
+	
+	public ArrayList<Agent> getAgents() { return agents; }
+    public Agent getAgent() { return agent; }
 
 }
