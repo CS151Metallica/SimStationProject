c42cebfe0cca1f6ab69bcca6bb0cfb36f7696bf6
letrung87 <letrung87@MiddleLee>
2020-04-09 18:49:24 -0700

    -Add SimStationPanel.java - Modify RandomWalkPanel.java and FlockingPanel.java to extends from SimStationPanel

--- SimStationProject/src/drunks/RandomWalkPanel.java
+++ SimStationProject/src/drunks/RandomWalkPanel.java
@@ -1,78 +1,12 @@
 package drunks;
 
-import java.awt.*;
-
-import javax.swing.*;
-import javax.swing.border.LineBorder;
-
 import mvc.*;
 import simstation.*;
 
-public class RandomWalkPanel extends AppPanel{
-
-	private JButton start, suspend, resume, stop, stats;
+public class RandomWalkPanel extends SimStationPanel{
 	
 	public RandomWalkPanel(AppFactory factory) {
 		super(factory);
-		
-		this.setLayout(new GridLayout(1, 2));
-        this.setPreferredSize(new Dimension(600, 600));
-		
-		SimulationView view = new SimulationView((Simulation)model);
-		view.setPreferredSize(new Dimension(255,255));		
-		
-		// panel to store all buttons: Leap, Clear
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setPreferredSize(new Dimension(100,250));
-        
-        // button Start
-        start = new JButton("Start");
-        start.addActionListener(this);
-        JPanel startPanel = new JPanel();
-        start.setPreferredSize(new Dimension(100, 30));
-        startPanel.add(start);
-        
-        // button Suspend
-        suspend = new JButton("Suspend");
-        suspend.addActionListener(this);
-        JPanel suspendPanel = new JPanel();
-        suspend.setPreferredSize(new Dimension(100, 30));
-        suspendPanel.add(suspend);
-        
-        // button Resume
-        resume = new JButton("Resume");
-        resume.addActionListener(this);
-        JPanel resumePanel = new JPanel();
-        resume.setPreferredSize(new Dimension(100, 30));
-        resumePanel.add(resume);
-        
-        // button Stop
-        stop = new JButton("Stop");
-        stop.addActionListener(this);
-        JPanel stopPanel = new JPanel();
-        stop.setPreferredSize(new Dimension(100, 30));
-        stopPanel.add(stop);
-        
-        // button Stats
-        stats = new JButton("Stats");
-        stats.addActionListener(this);
-        JPanel statsPanel = new JPanel();
-        stats.setPreferredSize(new Dimension(100, 30));
-        statsPanel.add(stats);   
-        
-        // add all buttons to buttonPanel
-        buttonPanel.add(start);
-        buttonPanel.add(suspend);
-        buttonPanel.add(resume);
-        buttonPanel.add(stop);
-        buttonPanel.add(stats);
-        
-		add(buttonPanel, "West");
-		add(view, "East");
-		
-		this.setLayout(new FlowLayout());
-        add(buttonPanel);
-        add(view); 
 	}
 	
 	public static void main(String[] args) {
---
+++ SimStationProject/src/simstation/SimStationPanel.java
@@ -0,0 +1,79 @@
+package simstation;
+
+import mvc.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class SimStationPanel extends AppPanel {
+	public JButton start, suspend, resume, stop, stats;
+	
+	public SimStationPanel(AppFactory factory) {
+		super(factory);
+		
+		this.setLayout(new GridLayout(1, 2));
+        this.setPreferredSize(new Dimension(600, 600));
+		
+		SimulationView view = new SimulationView((Simulation)model);
+		view.setPreferredSize(new Dimension(255,255));		
+		
+		// panel to store all buttons: Leap, Clear
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setPreferredSize(new Dimension(100,250));
+        
+        // button Start
+        start = new JButton("Start");
+        start.addActionListener(this);
+        JPanel startPanel = new JPanel();
+        start.setPreferredSize(new Dimension(100, 30));
+        startPanel.add(start);
+        
+        // button Suspend
+        suspend = new JButton("Suspend");
+        suspend.addActionListener(this);
+        JPanel suspendPanel = new JPanel();
+        suspend.setPreferredSize(new Dimension(100, 30));
+        suspendPanel.add(suspend);
+        
+        // button Resume
+        resume = new JButton("Resume");
+        resume.addActionListener(this);
+        JPanel resumePanel = new JPanel();
+        resume.setPreferredSize(new Dimension(100, 30));
+        resumePanel.add(resume);
+        
+        // button Stop
+        stop = new JButton("Stop");
+        stop.addActionListener(this);
+        JPanel stopPanel = new JPanel();
+        stop.setPreferredSize(new Dimension(100, 30));
+        stopPanel.add(stop);
+        
+        // button Stats
+        stats = new JButton("Stats");
+        stats.addActionListener(this);
+        JPanel statsPanel = new JPanel();
+        stats.setPreferredSize(new Dimension(100, 30));
+        statsPanel.add(stats);   
+        
+        // add all buttons to buttonPanel
+        buttonPanel.add(start);
+        buttonPanel.add(suspend);
+        buttonPanel.add(resume);
+        buttonPanel.add(stop);
+        buttonPanel.add(stats);
+        
+		add(buttonPanel, "West");
+		add(view, "East");
+		
+		this.setLayout(new FlowLayout());
+        add(buttonPanel);
+        add(view); 
+	}
+
+    public static void main(String[] args) {
+        AppFactory factory = new SimStationFactory();
+        AppPanel panel = new SimStationPanel(factory);
+        panel.display();
+    }
+}
